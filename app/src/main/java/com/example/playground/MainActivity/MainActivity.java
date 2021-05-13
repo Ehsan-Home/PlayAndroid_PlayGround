0d7d8d228094d1e9aa5b0c0566d237b972ded38b
ehsanghasaei <ehsan.ghasaei@gmail.com>
2021-05-12 20:54:12 -0700

    MainActivity rv and layout manager & adapter combined

--- app/src/main/java/com/example/playground/MainActivity/MainActivity.java
+++ app/src/main/java/com/example/playground/MainActivity/MainActivity.java
@@ -1,16 +1,37 @@
 package com.example.playground.MainActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.playground.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rv;
+    private ArrayList<MainActivityDataInstance> buttons = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rv = findViewById(R.id.mainactivity_rv);
+        fillButtons();
+        setUpRecyclerView(rv);
+    }
+
+    public void setUpRecyclerView(RecyclerView rv) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        RecyclerView.Adapter adapter = new MainActivityAdapter(buttons);
+        rv.setLayoutManager(layoutManager);
+        rv.setAdapter(adapter);
+    }
+
+    private void fillButtons () {
+        buttons.add(new MainActivityDataInstance("button 1",2));
+        buttons.add(new MainActivityDataInstance("button 2",2));
     }
 }
