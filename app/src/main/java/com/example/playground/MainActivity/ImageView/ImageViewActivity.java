2f6bfd77e45f7ab21ce07d6764f73c46105cada3
ehsanghasaei <ehsan.ghasaei@gmail.com>
2021-05-12 21:45:19 -0700

    image view activity & layout created

---
+++ app/src/main/java/com/example/playground/MainActivity/ImageView/ImageViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.playground.MainActivity.ImageView;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImageViewActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
