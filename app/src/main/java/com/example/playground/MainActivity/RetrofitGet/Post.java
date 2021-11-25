4e9f6866d2e3c40ff1f45a82c26aaf80fc003a03
ehsanghasaei <ehsan.ghasaei@gmail.com>
2021-11-25 09:40:03 -0800

    Post Java class created

---
+++ app/src/main/java/com/example/playground/MainActivity/RetrofitGet/Post.java
@@ -0,0 +1,31 @@
+package com.example.playground.MainActivity.RetrofitGet;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+
+    private String title;
+
+    @SerializedName("body")
+    private String text;
+
+    private int id;
+
+    private int userId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+}
