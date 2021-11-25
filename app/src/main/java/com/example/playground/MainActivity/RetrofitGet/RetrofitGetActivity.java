cfade7cf683de2a5a9ba1fcf84c588863e517b8c
ehsanghasaei <ehsan.ghasaei@gmail.com>
2021-11-25 10:21:51 -0800

    GSONFactory added and posts converted to string

--- app/src/main/java/com/example/playground/MainActivity/RetrofitGet/RetrofitGetActivity.java
+++ app/src/main/java/com/example/playground/MainActivity/RetrofitGet/RetrofitGetActivity.java
@@ -1,16 +1,59 @@
 package com.example.playground.MainActivity.RetrofitGet;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.playground.R;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitGetActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.retrofit_get_activity);
+
+
+        fetchPosts();
+    }
+
+    private void fetchPosts() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        Call<List<Post>> call = jsonPlaceHolderApi.fetchPosts();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    Log.e("getPosts", "Server respond back error: " + response.code());
+                    return;
+                }
+
+                List<Post> posts = response.body();
+                Log.d("getPosts", "posts : " + posts.toString());
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.e("getPosts", "error in communication with server : " + t.getMessage());
+            }
+        });
     }
 }
