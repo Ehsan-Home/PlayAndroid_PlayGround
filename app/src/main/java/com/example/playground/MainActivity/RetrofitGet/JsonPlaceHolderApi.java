ba3b9db54846e5a52e980b6d7038330d728718ee
ehsanghasaei <ehsan.ghasaei@gmail.com>
2021-11-26 08:12:19 -0800

    fetchComments interface class created

--- app/src/main/java/com/example/playground/MainActivity/RetrofitGet/JsonPlaceHolderApi.java
+++ app/src/main/java/com/example/playground/MainActivity/RetrofitGet/JsonPlaceHolderApi.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface JsonPlaceHolderApi {
 
     // relative URL for API call
     @GET("posts")
     Call<List<Post>> fetchPosts();
+
+    @GET("posts/{id}/comments")
+    // puts postId into id in path
+    Call<List<Comment>> fetchComments(@Path("id") int postId);
 }
